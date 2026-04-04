e60cb541ac9fbf0f6688ae7ab0ad126e440d44c9
credisulxaxim-afk <credisulxaxim@gmail.com>
2026-04-04 11:02:38 -0300

    add server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import fs from "fs";
+import https from "https";
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+
+// Carrega certificado (Base64 vindo do Railway)
+const certBuffer = Buffer.from(process.env.PIX_CERT_BASE64, "base64");
+
+const httpsAgent = new https.Agent({
+  pfx: certBuffer,
+  passphrase: process.env.PIX_CERT_PASSWORD
+});
+
+app.post("/pix", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://pix-h.api.efipay.com.br/v2/cob",
+      req.body,
+      {
+        httpsAgent,
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(
+              ${process.env.PIX_CLIENT_ID}:${process.env.PIX_CLIENT_SECRET}
+            ).toString("base64"),
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({
+      error: err.response?.data || err.message
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Proxy PIX rodando 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
